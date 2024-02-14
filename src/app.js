ce701564442b1ac3a037a27fbabcb0ed5e488c83
Manu214609 <150542974+Manu214609@users.noreply.github.com>
2024-02-14 20:04:05 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const who = ["the cat", "your turtle", "my dragon", "the neighbor"];
+const action = ["burned", "ate", "destroyed", "stole"];
+const what = ["my bed", "his car", "her napkin", "your house"];
+const when = ["this morning", "last nigth", "at 12", "yesterda"];
+
+function generateExcuse(a, b, c, d) {
+  let randomWho = a[Math.floor(Math.random() * 4)];
+  let randomAction = b[Math.floor(Math.random() * 4)];
+  let randomWhat = c[Math.floor(Math.random() * 4)];
+  let randomWhen = d[Math.floor(Math.random() * 4)];
+
+  return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+}
+
+let randomExcuse = generateExcuse(who, action, what, when);
+let selectorhtml = document.getElementById("excuse");
+selectorhtml.textContent = randomExcuse;
+
+document.getElementById("botonExcusa").addEventListener("click", () => {
+  location.reload();
+});
